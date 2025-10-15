0fe694e91cf919a79022927bd8793ca389617c0c
Castillo <antoniocj04@gmail.com>
2025-10-16 00:52:36 +0200

    Estructuras Secuenciales

---
+++ Ej_Estructuras_Secuenciales/ej1.py
@@ -0,0 +1,7 @@
+#
+# Programa que pregunta el nombre al usuario y luego lo saluda
+# Autor: Antonio Castillo Jiménez
+#
+
+name= input("Introduzca su nombre: ")
+print("Hola", name + ", bienvenido!!")
---
+++ Ej_Estructuras_Secuenciales/ej3.py
@@ -0,0 +1,12 @@
+#
+# Programa que calcula las horas y minutos correspondientes a una cantidad de minutos
+# Autor: Antonio Castillo Jiménez
+#
+print("Conversión de minutos a horas")
+
+minutos_introducidos = int(input("Introduzca los minutos a convertir: "))
+
+horas = minutos_introducidos // 60
+minutos = minutos_introducidos % 60
+
+print(minutos_introducidos, "minutos son", horas, "horas y", minutos, "minutos." )
